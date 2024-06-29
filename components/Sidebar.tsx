5523cb1ab9c2253cc0a8b3d42640730e471dd00b
Romanief <federicoromaniello@outlook.com>
2024-06-29 21:01:46 +0100

    Sidebar changes with user auth

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import { useUser } from '@/context/userContext'
+import Link from 'next/link'
 
 function Sidebar() {
+  const {user, setUser} = useUser()
   return (
     <aside className='flex flex-col gap-5 h-screen fixed p-10 '>
-      <div>Username</div>
-      <div>Your Tasks</div>
-      <div>Suggest a Task</div>
-      <div>Rewards</div>
-      <div>Settings</div>
-      <div>Logout</div>
+      {user ? (
+        <>
+          <div>{user.name}</div>
+          <div>Your Tasks</div>
+          <div>Suggest a Task</div>
+          <div>Rewards</div>
+          <div>Settings</div>
+          <div className='cursor-pointer' onClick={() => setUser(null)}>Logout</div>
+        </>
+        ) : (
+        <>
+          <Link href={"/auth/login"}>login</Link>
+          <Link href={"/auth/register"}>Register</Link>
+        </>
+        )
+        }
+
     </aside> 
   )
 }
