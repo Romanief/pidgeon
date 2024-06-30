d7ddc76bb0a1fad18053b1ab4aa83548774e6d9d
Romanief <federicoromaniello@outlook.com>
2024-06-30 14:00:50 +0100

    Replaced post

--- components/Post.tsx
+++ components/Post.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react'
 
 import { FaUserAlt } from "react-icons/fa";
+import { IoIosArrowForward } from "react-icons/io";
 
 function Post(
   {
@@ -10,11 +11,13 @@ function Post(
     description,
     heros, 
     herosNeeded = 5,
+    distance
   } : {
     title: string, 
     description: string,
     heros: number, 
     herosNeeded?: number,
+    distance: string
   }
   ) {
     const [selected, setSelected] = useState(false)
@@ -24,12 +27,23 @@ function Post(
     }
 
     return (
-      <div onClick={handleClick} className='fex flex-col border-b border-neutral-200 px-10 py-5 hover:bg-dirtyWhite cursor-pointer'>
-        <div className='flex text-2xl gap-5'>
-          <div className=''>{title}</div>
-          <div className='flex'><FaUserAlt className='text-xl mt-1 mr-1'/>: {selected ? heros + 1 : heros}/{herosNeeded}</div>
+      <div onClick={handleClick} className='flex cursor-pointer justify-between w-full h-28 bg-dirtWhite rounded-xl mb-3 shadow-md shadow-black/25'>
+        <div className='w-4/5 flex flex-col text-lg px-5 py-2'>
+        <div className='font-semibold'>{title}</div>
+          <div className='flex justify-between'>
+            <div>
+              <div className='pt-1'>{distance} miles away</div>
+              <div className='pt-1'>{description}</div>
+            </div>
+            <div className='flex flex-col'>
+              <div><span className='text-3xl font-semibold'>{heros}</span> of <span className='text-3xl font-semibold'>{herosNeeded}</span></div>
+              <div>signed up</div>
+            </div>
+          </div>
         </div>
-        <div>{description}</div>
+        <div className='bg-pPurple w-1/5 rounded-r-xl pt-5'>
+        <IoIosArrowForward className='text-dirtWhite text-7xl'/>
+        </div>
       </div>
     )
   }
